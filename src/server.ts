6e19149ce6a4ea368a612973d7a38e775ebbce8d
Maria Meir <pigyarx@gmail.com>
2023-11-01 14:42:42 -0400

    server.ts try catch

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,15 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
 
-const app = Fastify ({logger: true})
+const app = Fastify({ logger: true})
+
+const start = async () =>{
+
+  try{
+    await app.listen({port:3333})
+  }catch(err){
+    process.exit(1)
+  }
+}
+
+start();
