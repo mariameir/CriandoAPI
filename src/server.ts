9d8aac9664e418bf52204b94540826c1c6e6c711
Maria Meir <pigyarx@gmail.com>
2023-11-06 16:37:14 -0400

    prisma client config

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
+import { routes } from './routes';
 
 const app = Fastify({ logger: true})
 
+//startar o app
 const start = async () =>{
+
+  //carregar rotas
+  await app.register(cors);
+  await app.register(routes);
+
 
   try{
     await app.listen({port:3333})
