283692863e19728e59619069f6f49ee5d4567cf3
Maria Meir <pigyarx@gmail.com>
2023-11-13 13:51:29 -0400

    create service test

--- src/services/CreateCustomerService.ts
+++ src/services/CreateCustomerService.ts
@@ -1,2 +1,11 @@
+import { log } from "console";
 import prismaClient from "../prisma";
 
+class CreateCustumerService{
+  async execute(){
+    console.log("Rota foi chamada  ")
+    return {ok: true}
+  }
+}
+
+export { CreateCustumerService}
